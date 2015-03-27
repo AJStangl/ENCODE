913049e8a242afd290c1813a45a51521ac909055
AJStangl <ajstangl@gmail.com>
2015-03-27 12:03:01 -0700

    3/27/15

---
+++ key_value_encode.py
@@ -0,0 +1,26 @@
+import requests
+
+HEADERS = {'accept': 'application/json'}
+URL = "https://www.encodeproject.org/experiments/ENCSR000EMS/"
+response = requests.get(URL, headers=HEADERS)
+
+exp_dict = response.json()
+# with open ('key_dict.txt', 'w') as test:
+"""For Keys in general dict object"""
+
+
+"""Test for keys and values in files sub key"""
+i = 0
+limit = len(exp_dict["files"])
+while i < limit:
+    if exp_dict["files"][i]["file_format"] == "bam":
+        for k,v in exp_dict.iteritems():
+            print k
+            print v
+            print "\n"
+
+
+        print ("#########")
+        print ("\n")
+
+    i = i + 1
