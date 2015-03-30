368fdc518099918012f90bc868461f213bf73560
AJStangl <ajstangl@gmail.com>
2015-03-30 16:13:18 -0700

    3/27/15

--- encode_batch_uploader.py
+++ encode_batch_uploader.py
@@ -11,17 +11,14 @@ Requests the Authentication Token for Data Upload
 r = requests.post(auth_url, auth=("ajstangl", "Bio Informatics1"))
 auth_dict = r.json()
 token = auth_dict["result"]["token"]
-un = "ajstangl"
+username = "ajstangl"
 
 
+search = requests.get(base_url"/organisms/search/gid+?username=%s&token=%s") % (username, token)
+rep = search.json()
+print rep
 
 
-
-
-# payload = json.dumps({"name": "Dog_Shit","description": "Dog Fecal Matter Assay"})
-# headers = {'content-type': 'application/json'}
-# response = requests.post(base_url + "organisms",data=json.dumps(payload),headers=headers)
-# print response
 
 
 
