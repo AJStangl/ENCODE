91f3747c84ae71d66c678275e018680b258d7a5d
AJStangl <ajstangl@gmail.com>
2015-04-03 12:20:08 -0700

    3/27/15

--- download_files.py
+++ download_files.py
@@ -1,23 +1,30 @@
 __author__ = 'AJ'
-'''
-This function will access all files in the the enncode database, retreive the download
-url's in the the metadata file and download the files to the local directory.
-'''
+def download_bam(metadata):
+    '''
+    This function will access all files in the the enncode database, retrieve the download
+    url's in the the metadata file and download the files to the local directory.
 
-import urllib,csv
+    Input: A meta data file in TSV form that is written by bam_metadata_encode Script
 
-with open("bam_metadata_encode.tsv", 'r') as infile:
-    infile = csv.reader(infile, delimiter='\t')
+    Output: None
 
-    url = []
+    Return: None
+    '''
 
-    for row in infile:
-        if row[12] != "Download_Link":
-            url.append(row[12])
+    import urllib,csv
 
-    for elem in url:
-        fname = elem.split("/")
-        fname = fname[-1]
-        urllib.urlretrieve(elem, fname)
+    with open(metadata, 'r') as infile:
+        infile = csv.reader(infile, delimiter='\t')
 
+        url = []
+
+        for row in infile:
+            if row[12] != "Download_Link":
+                url.append(row[12])
+
+        for elem in url:
+            fname = elem.split("/")
+            fname = fname[-1]
+            urllib.urlretrieve(elem, fname)
+    return
 
